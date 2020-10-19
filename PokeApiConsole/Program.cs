6136e157dc6a392b771acc18437897fbf1c57c4a
Dillon Stricklett <dillonstricklett@live.com>
2020-10-18 17:13:12 -0700

    Created basic api client to retrieve pokemon by name

---
+++ PokeApiConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using PokeAPICore;
+using System;
+using System.Threading.Tasks;
+
+namespace PokeApiConsole
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            PokeApiClient client = new PokeApiClient();
+            string result = await client.GetPokemonByName("geodude");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
