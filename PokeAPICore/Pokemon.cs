de76b27ae4bb6da4a30949b17058811c25c9bbc2
Dillon Stricklett <dillonstricklett@live.com>
2020-10-31 17:36:36 -0700

    Issue#3 poke dex website (#6)
    * Added PokeAPI MVC Website
    
    * Created PokeDex view model
    
    * Added image to PokeDex view model
    
    * Create PokeDex page
    
    * Refactored Pokemon property names
    
    * Refactored

--- PokeAPICore/Pokemon.cs
+++ PokeAPICore/Pokemon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -13,8 +14,8 @@ namespace PokeAPICore
         private const double InchesPerDecimeter = 3.937;
         private int height1;
 
-        public int id { get; set; }
-        public string name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// Base experience gained from defeating this Pokemon.
@@ -24,7 +25,7 @@ namespace PokeAPICore
         /// <summary>
         /// The height of this Pokemon in inches.
         /// </summary>
-        public int height 
+        public int Height 
         { 
             // The height values are stored as decimeteres.
             get => (int)Math.Ceiling(height1 * InchesPerDecimeter); 
@@ -40,7 +41,7 @@ namespace PokeAPICore
         /// <summary>
         /// The weight of this Pokemon in hectograms.
         /// </summary>
-        public int weight { get; set; }
+        public int Weight { get; set; }
         public Ability[] abilities { get; set; }
         public Form[] forms { get; set; }
         public Game_Indices[] game_indices { get; set; }
@@ -48,7 +49,7 @@ namespace PokeAPICore
         public string location_area_encounters { get; set; }
         public Move[] moves { get; set; }
         public Species species { get; set; }
-        public Sprites sprites { get; set; }
+        public Sprites Sprites { get; set; }
         public Stat[] stats { get; set; }
         public Type[] types { get; set; }
     }
@@ -67,7 +68,9 @@ namespace PokeAPICore
         public string front_female { get; set; }
         public string front_shiny_female { get; set; }
         public string back_shiny { get; set; }
-        public string front_default { get; set; }
+
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
         public string front_shiny { get; set; }
         public Other other { get; set; }
         public Versions versions { get; set; }
---
+++ PokeApiWebsite/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokeAPICore;
+using PokeApiWebsite.Models;
+
+namespace PokeApiWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            PokeApiClient client = new PokeApiClient();
+            Pokemon result = await client.GetPokemonById(1);
+
+            List<string> resultMoves = new List<string>();
+            foreach(Move currMove in result.moves)
+            {
+                resultMoves.Add(currMove.move.name);
+            }
+
+            resultMoves.Sort();
+
+            //if it were a list of Pokemon
+            // temp.OrderBy(p => p.name);
+
+            PokeDexEntryViewModel entry = new PokeDexEntryViewModel()
+            {
+                Id = result.Id,
+                Name = result.Name,
+                Height = result.Height.ToString(),
+                Weight = result.Weight.ToString(),
+                PokeDexImageUrl = result.Sprites.FrontDefault,
+                MoveList = resultMoves
+            };
+
+            return View(entry);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
